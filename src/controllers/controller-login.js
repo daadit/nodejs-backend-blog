7fa927a3b7394c7c9210920f6d97688d1d2c52f3
adilsanak <adilsaputra0101@gmail.com>
2022-04-20 09:52:58 +0700

    Home

--- src/controllers/controller-login.js
+++ src/controllers/controller-login.js
@@ -1,20 +1,20 @@
-const config = require('../configs/database');
+const config = require("../configs/database");
 
-let mysql = require('mysql');
+let mysql = require("mysql");
 let pool = mysql.createPool(config);
 
-pool.on('error', (err) => {
+pool.on("error", (err) => {
     console.error(err);
 });
 
 module.exports = {
     // Render tampilan untuk login yang ada didalam folder 'src/views/login.ejs'
     login(req, res) {
-        res.render('login', {
-            url: 'http://localhost:8080/',
+        res.render("login", {
+            url: "http://localhost:8080/",
             // Kirim juga library flash yang telah di set
-            colorFlash: req.flash('color'),
-            pesanFlash: req.flash('message'),
+            colorFlash: req.flash("color"),
+            pesanFlash: req.flash("message"),
         });
     },
     // Post / kirim data yang diinput user
@@ -24,25 +24,25 @@ module.exports = {
         if (email && password) {
             pool.getConnection(function (err, connection) {
                 if (err) throw err;
-                connection.query(`SELECT * FROM t_user WHERE email = ? AND password = ?`, [email, password], function (error, results) {
+                connection.query(`SELECT * FROM tb_user WHERE email = ? AND password = ?`, [email, password], function (error, results) {
                     if (error) throw error;
                     if (results.length > 0) {
                         // Jika data ditemukan, set sesi user tersebut menjadi true
                         req.session.loggedin = true;
                         req.session.userid = results[0].id_user;
                         req.session.username = results[0].fullname;
-                        res.redirect('/');
+                        res.redirect("/");
                     } else {
                         // Jika data tidak ditemukan, set library flash dengan pesan error yang diinginkan
-                        req.flash('color', 'danger');
-                        req.flash('message', 'Wrong password!');
-                        res.redirect('/login');
+                        req.flash("color", "danger");
+                        req.flash("message", "Wrong password!");
+                        res.redirect("/login");
                     }
                 });
                 connection.release();
             });
         } else {
-            res.redirect('/login');
+            res.redirect("/login");
             res.end();
         }
     },
@@ -54,8 +54,8 @@ module.exports = {
                 return console.log(err);
             }
             // Hapus cokie yang masih tertinggal
-            res.clearCookie('secretname');
-            res.redirect('/login');
+            res.clearCookie("secretname");
+            res.redirect("/login");
         });
     },
 };
